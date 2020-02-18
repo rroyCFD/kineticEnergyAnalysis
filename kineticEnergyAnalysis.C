8bfd93d7aaf9322233eb69fcaaa3893bf65c1e31
rroyCFD <roy.rajib@live.com>
2020-02-18 14:48:40 -0700

    replaced sum with weighted-average; writing into a log file

--- kineticEnergyAnalysis.C
+++ kineticEnergyAnalysis.C
@@ -34,6 +34,7 @@ namespace Foam
 
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
 tmp<volScalarField> Foam::kineticEnergyAnalysis::getTemporalKE()
 {
     tmp<volScalarField> tTemporalKE
@@ -54,8 +55,12 @@ tmp<volScalarField> Foam::kineticEnergyAnalysis::getTemporalKE()
     volScalarField& temporalKE = tTemporalKE.ref();
     temporalKE.write(runTime_.outputTime());
 
-    scalar sumTemporalKE = gSum(temporalKE.primitiveFieldRef() * mesh_.V());
-    Info << "sum of temporalDerivative KE: " << sumTemporalKE << endl;
+    // scalar sumTemporalKE = gSum(temporalKE.primitiveFieldRef() * mesh_.V());
+    // Info << "\tsum of temporalDerivative KE: " << sumTemporalKE << endl;
+
+    // volume weighted average
+    avgDdtKE_ = temporalKE.weightedAverage(mesh_.V());
+    Info << "\tavg of temporalDerivative KE: " << avgDdtKE_.value()<< endl;
 
     return tTemporalKE;
 }
@@ -80,8 +85,12 @@ tmp<volScalarField> Foam::kineticEnergyAnalysis::getConvectionKE()
     volScalarField& convKE = tConvKE.ref();
     convKE.write(runTime_.outputTime());
 
-    scalar sumConvKE = gSum(convKE.primitiveFieldRef() * mesh_.V());
-    Info << "sum of convection KE: " << sumConvKE << endl;
+    // scalar sumConvKE = gSum(convKE.primitiveFieldRef() * mesh_.V());
+    // Info << "\tsum of convection KE: " << sumConvKE << endl;
+
+    // volume weighted average
+    avgConvKE_ = convKE.weightedAverage(mesh_.V());
+    Info << "\tavg of convection KE: " << avgConvKE_.value() << endl;
 
     return tConvKE;
 }
@@ -106,7 +115,7 @@ tmp<volScalarField> Foam::kineticEnergyAnalysis::getPGradKE()
             ),
             // mesh_,
             // dimensionedScalar("", dimensionSet(0, 2, -3, 0, 0, 0, 0), 0)
-            (U_ & fvc::grad(p_)) // Approach 1
+            -(U_ & fvc::grad(p_)) // Approach 1
             // (U_ & fvc::reconstruct(fvc::snGrad(p_)* mesh_.magSf())) // Approach 2
             // (U_ & pGradLS_) // Approach 3
         )
@@ -117,8 +126,12 @@ tmp<volScalarField> Foam::kineticEnergyAnalysis::getPGradKE()
     volScalarField& pGradKE = tPGradKE.ref();
     pGradKE.write(runTime_.outputTime());
 
-    scalar sumPGradKE = gSum(pGradKE.primitiveFieldRef() * mesh_.V());
-    Info << "sum of pGrad KE: " << sumPGradKE << endl;
+    // scalar sumPGradKE = gSum(pGradKE.primitiveFieldRef() * mesh_.V());
+    // Info << "\tsum of pGrad KE: " << sumPGradKE << endl;
+
+    // volume weighted average
+    avgGradpKE_ = pGradKE.weightedAverage(mesh_.V());
+    Info << "\tavg of pGrad KE: " << avgGradpKE_.value() << endl;
 
     return tPGradKE;
 }
@@ -157,13 +170,17 @@ tmp<volScalarField> Foam::kineticEnergyAnalysis::getDissipationKE()
     volScalarField& dissipKE = tDissipKE.ref();
     dissipKE.write(runTime_.outputTime());
 
-    scalar sumDissipKE = gSum(dissipKE.primitiveFieldRef() * mesh_.V());
-    Info << "sum of dissipation KE: " << sumDissipKE << endl;
+    // scalar sumDissipKE = gSum(dissipKE.primitiveFieldRef() * mesh_.V());
+    // Info << "\tsum of dissipation KE: " << sumDissipKE << endl;
+
+    // volume weighted average
+    avgDisspKE_ = dissipKE.weightedAverage(mesh_.V());
+    Info << "\tavg of dissipation KE: " << avgDisspKE_.value() << endl;
 
     return tDissipKE;
 }
 
-
+// public member functions ---------------------------------------------------//
 void Foam::kineticEnergyAnalysis::analyzeKEBalance()
 {
     // debug purpose: code check!
@@ -189,7 +206,7 @@ void Foam::kineticEnergyAnalysis::analyzeKEBalance()
             // dimensionedScalar("", dimensionSet(0, 2, -3, 0, 0, 0, 0), 0)
             (  getTemporalKE()
              + getConvectionKE()
-             + getPGradKE()
+             - getPGradKE()
              - getDissipationKE()
             )
         )
@@ -205,8 +222,12 @@ void Foam::kineticEnergyAnalysis::analyzeKEBalance()
     volScalarField& KEbalance = tKEbalance.ref();
     KEbalance.write(runTime_.outputTime());
 
-    scalar sumKEbalance = gSum(KEbalance.primitiveFieldRef() * mesh_.V());
-    Info << "sum of KE balance: " << sumKEbalance << endl;
+    // scalar sumKEbalance = gSum(KEbalance.primitiveFieldRef() * mesh_.V());
+    // Info << "\tsum of KE balance: " << sumKEbalance << endl;
+
+    // avgKEBalance_ = (avgDdtKE_+avgConvKE_+avgGradpKE_+avgDisspKE_);
+    avgKEBalance_ = KEbalance.weightedAverage(mesh_.V());
+    Info << "\tavg of KE balance: " << avgKEBalance_.value() << endl;
 
     return;
 }
@@ -242,12 +263,98 @@ void Foam::kineticEnergyAnalysis::getPPGradDiffKE()
     volScalarField& ppGradDiffKE = tppGradDiffKE.ref();
     ppGradDiffKE.write(runTime_.outputTime());
 
-    scalar sumPPGradDiffKE = gSum(ppGradDiffKE.primitiveFieldRef() * mesh_.V());
-    Info << "sum of ppGradDiff KE: " << sumPPGradDiffKE << endl;
+    // scalar sumPPGradDiffKE = gSum(ppGradDiffKE.primitiveFieldRef() * mesh_.V());
+    // Info << "\tsum of ppGradDiff KE: " << sumPPGradDiffKE << endl;
+
+    // volume weighted average
+    avgPpGradKE_ = ppGradDiffKE.weightedAverage(mesh_.V());
+    Info << "\tavg of ppGradDiff KE: " << avgPpGradKE_.value() << endl;
 
     return;
 }
 
+
+void Foam::kineticEnergyAnalysis::setPropertiesOutput()
+{
+    if(Pstream::parRun() && !(Pstream::master()))
+    {
+        return;
+    }
+
+    // create output file
+    fileName outputDir;
+    autoPtr<fileName> outFilePath_;
+
+    if(Pstream::parRun() && Pstream::master())
+    {
+        outputDir = runTime_.path()/"../postProcessing";
+
+        outFilePath_.reset( new fileName
+        (
+            runTime_.path()/"../postProcessing"/
+            ("kineticEnergyAnalysis.dat_"+runTime_.timeName())
+        )
+        );
+
+    }
+    else
+    {
+        outputDir = runTime_.path()/"postProcessing";
+
+        outFilePath_.reset( new fileName
+        (
+            runTime_.path()/"postProcessing"/
+            ("kineticEnergyAnalysis.dat_"+runTime_.timeName())
+        )
+        );
+    }
+
+    if (!isDir(outputDir))
+    {
+        mkDir(outputDir);
+    }
+
+    KEPropertiesFile_.reset(new OFstream(*outFilePath_));
+
+    // write header of the log file
+    KEPropertiesFile_()
+        << "time" << tab
+        << "avgKEBalance(t)" << tab
+        << "avgDdtKE(t)" << tab
+        << "avgConvKE(t)" << tab
+        << "avgGradpKE(t)" << tab
+        << "avgDisspKE(t)" << tab
+        << "avgPpGradKE(t)" << endl;
+
+    KEPropertiesFile_().precision(12); // set precision
+
+    Info << "KE analysis: Global properties are written in\n"
+         << KEPropertiesFile_().name() << endl;
+
+    return;
+}
+
+
+void Foam::kineticEnergyAnalysis::writeAvgValues()
+{
+    if(Pstream::parRun() && !(Pstream::master()))
+    {
+        return;
+    }
+
+    Info << "Writing to KE-log file" << endl;
+
+    KEPropertiesFile_()
+        << runTime_.value() << tab
+        << avgKEBalance_.value() << tab
+        << avgDdtKE_.value() << tab
+        << avgConvKE_.value() << tab
+        << avgGradpKE_.value() << tab
+        << avgDisspKE_.value() << tab
+        << avgPpGradKE_.value() << endl;
+
+    return;
+}
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
@@ -267,9 +374,19 @@ Foam::kineticEnergyAnalysis::kineticEnergyAnalysis
     // Set the pointer to the velocity, flux and pressure-correction field
     U_(U),
     phi_(phi),
-    p_(p)
-{
+    p_(p),
 
+    avgKEBalance_("avgKE",       dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0),
+    avgDdtKE_    ("avgDdtKE",    dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0),
+    avgConvKE_   ("avgConvKE",   dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0),
+    avgGradpKE_  ("avgGradpKE",  dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0),
+    avgDisspKE_  ("avgDisspKE",  dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0),
+    avgPpGradKE_ ("avgPpGradKE", dimensionSet(0, 2, -3, 0, 0, 0, 0), 0.0)
+{
+    // setPropertiesOutput();
+    // analyzeKEBalance();
+    // getPPGradDiffKE();
+    // writeAvgValues();
 }
 
 // * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
